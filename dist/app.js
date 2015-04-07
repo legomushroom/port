50b9aba13c9040a005d835278b8eb26da1b438c4
Oleg Solomka <legomushroom@gmail.com>
2015-04-07 20:39:33 +0300

    amend

--- dist/app.js
+++ dist/app.js
@@ -80,13 +80,13 @@
 	  function Main(o) {
 	    this.vars();
 	    this.initContainer();
-	    // this.createParticles()
 	    this.draw();
 	  }
 
 	  _prototypeProperties(Main, null, {
 	    vars: {
 	      value: function vars() {
+	        var _this = this;
 	        this.particlesContainer = document.querySelector("#js-particles");
 	        this.particles = document.querySelectorAll(".particle");
 	        this.badge = document.querySelector("#js-badge");
@@ -94,13 +94,13 @@
 	        var styles = getComputedStyle(this.particlesContainer);
 	        this.width = parseInt(styles.width, 10);
 	        this.height = parseInt(styles.height, 10);
-	        this.wWidth = window.innerWidth;
-	        this.wHeight = window.innerHeight;
-	        this.centerY = this.height / 2 - this.wHeight / 2;
-	        this.centerX = this.width / 2 - this.wWidth / 2;
 	        this.radPoint = mojs.helpers.getRadialPoint;
-	        this.bubleCenter = { x: this.centerX, y: this.centerY };
 	        this.particleBuffer = null;
+	        this.calcDimentions();
+	        window.addEventListener("resize", function () {
+	          return _this.calcDimentions();
+	        });
+	        // document.addEventListener('mouseout', (e) => this.jelly(e) );
 
 	        var i = this.particlesLength;
 	        while (i--) {
@@ -108,25 +108,52 @@
 	          particle.x = parseInt(particle.getAttribute("data-left"), 10);
 	          particle.y = parseInt(particle.getAttribute("data-top"), 10);
 	        }
-	        //     // Create a physics instance which uses the Verlet integration method
-	        //     this.physics = new Physics();
-	        //     this.physics.integrator  = new Verlet();
-	        //     this.particlesContainer      = document.querySelectorAll('.particle');
-	        //     // this.avoidMouse     = new Attraction();
-	        //     this.pullToCenter   = new Attraction();
-	        //     this.collision      = new Collision();
-	        //     this.PARTICLE_SIZE  = 75;
-	        //     this.PARTICLE_PERCENT  = this.PARTICLE_SIZE/100;
-	        //     this.GAP = 15
-	        //     this.PARTICLE_SIZE_G= this.PARTICLE_SIZE + this.GAP;
-	        //     this.CENTER_SIZE    = 2.12*this.PARTICLE_SIZE;
-	        //     // this.CENTER_SIZE    = 3*this.PARTICLE_SIZE;
-	        //     this.width = 1*1440; this.height = 1*800
-	        //     this.centerX = this.width/2
-	        //     this.centerY = this.height/2
-
-	        //     this.centerXOrigin = this.width/2
-	        //     this.centerYOrigin = this.height/2
+	      },
+	      writable: true,
+	      enumerable: true,
+	      configurable: true
+	    },
+	    jelly: {
+	      value: function jelly(e) {
+	        var el = e.target;
+	        if (el.classList.contains("particle")) {
+	          el = el.childNode;
+	        } else if (!el.classList.contains("particle__inner")) {
+	          return;
+	        }
+	        var tween = new mojs.Tween();
+	        var coef1 = 0;var coef2 = 0;var angle = 0;
+	        var timeline = new mojs.Timeline({
+	          duration: 1500,
+	          easing: "elastic.out",
+	          onStart: function () {
+	            coef1 = mojs.helpers.rand(-2, 2) / 10;
+	            coef2 = 0.2 * Math.random();
+	          },
+	          onUpdate: function (p) {
+	            var np = 1 - p;
+	            var transform = "scale(" + (1 + coef1 * np) + "," + (1 - coef2 + coef2 * p) + ") translateZ(0)";
+	            if (!el) return;
+	            mojs.helpers.setPrefixedStyle(el, "transform", transform);
+	            // console.log(p)
+	          }
+	        });
+	        tween.add(timeline);
+	        tween.start();
+	      },
+	      writable: true,
+	      enumerable: true,
+	      configurable: true
+	    },
+	    calcDimentions: {
+	      value: function calcDimentions() {
+	        this.wWidth = window.innerWidth;
+	        this.wHeight = window.innerHeight;
+	        this.centerY = this.height / 2 - this.wHeight / 2;
+	        this.centerX = this.width / 2 - this.wWidth / 2;
+	        this.bubleCenter = { x: this.centerX, y: this.centerY };
+	        // this.size = Math.sqrt((this.wHeight*this.wHeight) + (this.wWidth*this.wWidth)*(this.wHeight/this.wWidth))
+	        this.size = Math.min(Math.sqrt(this.wHeight * this.wHeight), Math.sqrt(this.wWidth * this.wWidth));
 	      },
 	      writable: true,
 	      enumerable: true,
@@ -157,69 +184,34 @@
 	          this.particleBuffer = this.particles[i];
 	          var x = Math.abs(this.bubleCenter.x - this.particleBuffer.x);
 	          var y = Math.abs(this.bubleCenter.y - this.particleBuffer.y);
-	          var delta = Math.sqrt(2448400) / (Math.sqrt(x * x + y * y) * 5);
+
+	          var delta = this.size / (Math.sqrt(x * x + y * y) * 2.6);
 	          var radius = Math.sqrt(x * x + y * y);
 	          // this.badge.textContent = delta
-	          // this.particleBuffer.textContent = delta
-	          delta = Math.min(delta, 1);
-	          delta = Math.max(delta, 0);
+	          var shift = delta > 1.4 ? (delta - 1.4) / 14 : 0;
+	          shift = mojs.helpers.clamp(shift, 0, 1.006);
+	          delta = Math.min(delta, 1);delta = Math.max(delta, 0);
 	          delta = mojs.easing.exponential["in"](delta);
-	          delta = delta.toFixed(2);
+	          // delta = (mojs.easing.back.in(delta))
 	          delta = Math.max(delta, 0.03);
+	          // delta += mojs.easing.cubic.out(shift)
+	          // delta = Math.min(delta, 1.08)
+	          delta = delta.toFixed(2);
+	          // console.log()
+	          // this.particleBuffer.children[0].textContent = delta
 	          if (this.particleBuffer.prevDelta !== delta) {
 	            cnt++;
-	            // get angle
-	            // var dX = this.particleBuffer.x-this.bubleCenter.x
-	            // var dY = this.particleBuffer.y-this.bubleCenter.y
-	            // var angle = (Math.atan(dY/dX)*(180/Math.PI))-90
-	            // if (this.bubleCenter.x-this.particleBuffer.x < 0){
-	            //   angle = 180 + angle;
-	            // }
-	            // var point = this.radPoint({
-	            //   angle: angle,
-	            //   radius: radius + 80*(1-mojs.easing.cubic.in(delta)),
-	            //   center: { x: this.bubleCenter.x, y: this.bubleCenter.y}
-	            // });
-
-	            // var xShift = this.particleBuffer.x - point.x;
-	            // var yShift = this.particleBuffer.y - point.y;
-	            // var translate = `translate3d(${xShift}px, ${yShift}px, 0)`
 	            var nDelta = mojs.easing.exponential["in"](1 - delta);
-	            var transform = "scale(" + delta + ") translateZ(" + -150 * nDelta + "px)";
+	            var translateZ = -175 * nDelta;
+	            var transform = "scale(" + delta + ") translateZ(" + translateZ + "px)";
 	            mojs.helpers.setPrefixedStyle(this.particleBuffer, "transform", transform);
-	            // this.particleBuffer.style.left = point.x + 'px'
-	            // this.particleBuffer.style.top = point.y + 'px'
-	            // this.particleBuffer.textContent = angle.toFixed(2)
-	            // this.particleBuffer.style.transform = transform;
 	            this.particleBuffer.prevDelta = delta;
+	            this.particleBuffer.translateZ = translateZ;
+	            this.particleBuffer.delta = delta;
+	            this.particleBuffer.nDelta = nDelta;
 	          }
-
 	          // this.badge.textContent = cnt;
 	        }
-
-	        //         // var delta2 = (this.height/3)/Math.abs(this.centerY - particle.pos.y)
-	        //         // delta2 = Math.min(delta2, 1);
-	        //         // delta2 = Math.max(delta2, 0.1);
-
-	        //         var x = Math.abs(this.centerX - particle.pos.x)
-	        //         var y = Math.abs(this.centerY - particle.pos.y)
-	        //         var delta = Math.sqrt(2448400)/(Math.sqrt(x*x + y*y)*6)
-	        //         delta = Math.min(delta, 1);
-	        //         delta = Math.max(delta, 0.1);
-	        //         // console.log(delta)
-
-	        //         delta = mojs.easing.quadratic.in(mojs.easing.exponential.in(delta))
-
-	        //         particle.radius2 = this.PARTICLE_SIZE_G*delta
-	        //         // particle.radius2 = this.PARTICLE_SIZE_G*delta2
-
-	        //         var transform = `translate(${particle.pos.x}px, ${particle.pos.y}px) scale(${delta})`;
-	        //         this.particlesContainer[i].style.transform = transform;
-	        //         // this.particlesContainer[i].style.width = `${2*particle.radius2 - 2*this.GAP}px`;
-	        //         // this.particlesContainer[i].style.height = `${2*particle.radius2 - 2*this.GAP}px`;
-	        //         // this.particlesContainer[i].style['margin-left'] = `${-particle.radius2/2}px`;
-	        //         // this.particlesContainer[i].style['margin-top'] = `${-particle.radius2/2}px`;
-	        //     }
 	        requestAnimationFrame(this.draw.bind(this));
 	      },
 	      writable: true,
@@ -231,14 +223,7 @@
 	  return Main;
 	})();
 
-	window.app = new Main
-
-
-
-	// setTimeout(function () {
-	//   window.app.saveParticles()
-	// }, 20000)
-	(); // this.listenScroll()
+	window.app = new Main();
 
 /***/ },
 /* 2 */
--- js/app.es6
+++ js/app.es6
@@ -14,9 +14,7 @@ class Main {
   constructor(o) {
     this.vars()
     this.initContainer()
-    // this.createParticles()
     this.draw()
-    // this.listenScroll()
   }
   vars() {
     this.particlesContainer = document.querySelector('#js-particles');
@@ -26,13 +24,11 @@ class Main {
     var styles     = getComputedStyle(this.particlesContainer);
     this.width     = parseInt(styles.width, 10);
     this.height    = parseInt(styles.height,10);
-    this.wWidth    = window.innerWidth;
-    this.wHeight   = window.innerHeight;
-    this.centerY   = this.height/2 - this.wHeight/2;
-    this.centerX   = this.width/2 - this.wWidth/2;
     this.radPoint = mojs.helpers.getRadialPoint;
-    this.bubleCenter = { x: this.centerX, y: this.centerY }
     this.particleBuffer = null;
+    this.calcDimentions()
+    window.addEventListener('resize', () => this.calcDimentions())
+    // document.addEventListener('mouseout', (e) => this.jelly(e) );
 
     var i = this.particlesLength;
     while(i--) {
@@ -40,25 +36,43 @@ class Main {
       particle.x = parseInt(particle.getAttribute('data-left'), 10);
       particle.y = parseInt(particle.getAttribute('data-top'),  10);
     }
-//     // Create a physics instance which uses the Verlet integration method
-//     this.physics = new Physics();
-//     this.physics.integrator  = new Verlet();
-//     this.particlesContainer      = document.querySelectorAll('.particle');
-//     // this.avoidMouse     = new Attraction();
-//     this.pullToCenter   = new Attraction();
-//     this.collision      = new Collision();
-//     this.PARTICLE_SIZE  = 75;
-//     this.PARTICLE_PERCENT  = this.PARTICLE_SIZE/100;
-//     this.GAP = 15
-//     this.PARTICLE_SIZE_G= this.PARTICLE_SIZE + this.GAP;
-//     this.CENTER_SIZE    = 2.12*this.PARTICLE_SIZE;
-//     // this.CENTER_SIZE    = 3*this.PARTICLE_SIZE;
-//     this.width = 1*1440; this.height = 1*800
-//     this.centerX = this.width/2
-//     this.centerY = this.height/2
+  }
+  jelly (e) {
+    var el = e.target;
+    if (el.classList.contains('particle')) {
+      el = el.childNode
+    } else if (!el.classList.contains('particle__inner')) {
+      return
+    }
+    var tween = new mojs.Tween;
+    var coef1 = 0; var coef2 = 0; var angle = 0
+    var timeline = new mojs.Timeline({
+      duration: 1500,
+      easing:   'elastic.out',
+      onStart: function () {
+        coef1 = mojs.helpers.rand(-2,2)/10;
+        coef2 = .2*Math.random();
+      },
+      onUpdate: function (p) {
+        var np = 1-p;
+        var transform = `scale(${1+(coef1*np)},${(1-coef2)+(coef2*p)}) translateZ(0)`;
+        if (!el) return
+        mojs.helpers.setPrefixedStyle(el, 'transform', transform);
+        // console.log(p)
+      }
+    });
+    tween.add(timeline)
+    tween.start()
 
-//     this.centerXOrigin = this.width/2
-//     this.centerYOrigin = this.height/2
+  }
+  calcDimentions () {
+    this.wWidth    = window.innerWidth;
+    this.wHeight   = window.innerHeight;
+    this.centerY   = this.height/2 - this.wHeight/2;
+    this.centerX   = this.width/2 - this.wWidth/2;
+    this.bubleCenter = { x: this.centerX, y: this.centerY }
+    // this.size = Math.sqrt((this.wHeight*this.wHeight) + (this.wWidth*this.wWidth)*(this.wHeight/this.wWidth))
+    this.size = Math.min(Math.sqrt(this.wHeight*this.wHeight), Math.sqrt(this.wWidth*this.wWidth))
   }
   initContainer () {
     var it = this;
@@ -79,79 +93,37 @@ class Main {
       this.particleBuffer = this.particles[i];
       var x = Math.abs(this.bubleCenter.x-this.particleBuffer.x)
       var y = Math.abs(this.bubleCenter.y-this.particleBuffer.y)
-      var delta = Math.sqrt(2448400)/(Math.sqrt(x*x + y*y)*5)
-      var radius = Math.sqrt(x*x + y*y)
-      // this.badge.textContent = delta
-      // this.particleBuffer.textContent = delta
-      delta = Math.min(delta, 1.0);
-      delta = Math.max(delta, 0);
-      delta = (mojs.easing.exponential.in(delta))
-      delta = delta.toFixed(2)
-      delta = Math.max(delta, 0.03);
-      if (this.particleBuffer.prevDelta !== delta){
-        cnt++;
-        // get angle
-        // var dX = this.particleBuffer.x-this.bubleCenter.x
-        // var dY = this.particleBuffer.y-this.bubleCenter.y
-        // var angle = (Math.atan(dY/dX)*(180/Math.PI))-90
-        // if (this.bubleCenter.x-this.particleBuffer.x < 0){
-        //   angle = 180 + angle;
-        // }
-        // var point = this.radPoint({
-        //   angle: angle,
-        //   radius: radius + 80*(1-mojs.easing.cubic.in(delta)),
-        //   center: { x: this.bubleCenter.x, y: this.bubleCenter.y}
-        // });
-        
-        // var xShift = this.particleBuffer.x - point.x;
-        // var yShift = this.particleBuffer.y - point.y;
-        // var translate = `translate3d(${xShift}px, ${yShift}px, 0)`
-        var nDelta = mojs.easing.exponential.in(1-delta)
-        var transform = `scale(${delta}) translateZ(${-150*(nDelta)}px)`;
-        mojs.helpers.setPrefixedStyle(this.particleBuffer, 'transform', transform);
-        // this.particleBuffer.style.left = point.x + 'px'
-        // this.particleBuffer.style.top = point.y + 'px'
-        // this.particleBuffer.textContent = angle.toFixed(2)
-        // this.particleBuffer.style.transform = transform;
-        this.particleBuffer.prevDelta = delta
-      }
 
+      var delta = this.size/(Math.sqrt(x*x + y*y)*2.6);
+      var radius = Math.sqrt(x*x + y*y);
+      // this.badge.textContent = delta
+      var shift = (delta > 1.4) ? (delta-1.4)/14 : 0
+      shift = mojs.helpers.clamp(shift, 0, 1.006)
+      delta = Math.min(delta, 1); delta = Math.max(delta, 0);
+      delta = (mojs.easing.exponential.in(delta))
+      // delta = (mojs.easing.back.in(delta))
+      delta = Math.max(delta, 0.03);
+      // delta += mojs.easing.cubic.out(shift)
+      // delta = Math.min(delta, 1.08)
+      delta = delta.toFixed(2);
+      // console.log()
+      // this.particleBuffer.children[0].textContent = delta
+      if (this.particleBuffer.prevDelta !== delta) {
+        cnt++;
+        var nDelta = mojs.easing.exponential.in(1-delta);
+        var translateZ = -175*(nDelta);
+        var transform = `scale(${delta}) translateZ(${translateZ}px)`;
+        mojs.helpers.setPrefixedStyle(this.particleBuffer, 'transform', transform);
+        this.particleBuffer.prevDelta = delta
+        this.particleBuffer.translateZ = translateZ
+        this.particleBuffer.delta  = delta
+        this.particleBuffer.nDelta = nDelta
+      }
       // this.badge.textContent = cnt;
     }
-
-//         // var delta2 = (this.height/3)/Math.abs(this.centerY - particle.pos.y)
-//         // delta2 = Math.min(delta2, 1);
-//         // delta2 = Math.max(delta2, 0.1);
-
-//         var x = Math.abs(this.centerX - particle.pos.x)
-//         var y = Math.abs(this.centerY - particle.pos.y)
-//         var delta = Math.sqrt(2448400)/(Math.sqrt(x*x + y*y)*6)
-//         delta = Math.min(delta, 1);
-//         delta = Math.max(delta, 0.1);
-//         // console.log(delta)
-
-//         delta = mojs.easing.quadratic.in(mojs.easing.exponential.in(delta))
-
-//         particle.radius2 = this.PARTICLE_SIZE_G*delta
-//         // particle.radius2 = this.PARTICLE_SIZE_G*delta2
-
-//         var transform = `translate(${particle.pos.x}px, ${particle.pos.y}px) scale(${delta})`;
-//         this.particlesContainer[i].style.transform = transform;
-//         // this.particlesContainer[i].style.width = `${2*particle.radius2 - 2*this.GAP}px`;
-//         // this.particlesContainer[i].style.height = `${2*particle.radius2 - 2*this.GAP}px`;
-//         // this.particlesContainer[i].style['margin-left'] = `${-particle.radius2/2}px`;
-//         // this.particlesContainer[i].style['margin-top'] = `${-particle.radius2/2}px`;
-//     }
     requestAnimationFrame(this.draw.bind(this));
   }
 }
 
 window.app = new Main
-
-
-
-// setTimeout(function () {
-//   window.app.saveParticles()
-// }, 20000)
-
 
